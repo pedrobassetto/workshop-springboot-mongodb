8fc5d67e79e8c0b3243a1e3b6d1927b9d5e13cf6
Pedro Bassetto <contato@pedrobassetto.com>
2020-05-11 20:07:59 -0300

    Conectando ao MongoDB com com repository e service

---
+++ src/main/java/com/pedrobassetto/workshopmongo/resources/UserResource.java
@@ -0,0 +1,25 @@
+package com.pedrobassetto.workshopmongo.resources;
+
+import com.pedrobassetto.workshopmongo.domain.User;
+import com.pedrobassetto.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value= "/users")
+public class UserResource {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+}
