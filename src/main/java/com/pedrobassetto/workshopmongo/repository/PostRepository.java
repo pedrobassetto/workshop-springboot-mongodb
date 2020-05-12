039b9d9cac930fdbd0c9a4376eb67cf8746ea56f
Pedro Bassetto <contato@pedrobassetto.com>
2020-05-12 20:38:16 -0300

    Consulta com @Query

--- src/main/java/com/pedrobassetto/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/pedrobassetto/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.pedrobassetto.workshopmongo.repository;
 import com.pedrobassetto.workshopmongo.domain.Post;
 import com.pedrobassetto.workshopmongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +12,8 @@ import java.util.List;
 public interface PostRepository extends MongoRepository<Post, String> {
 
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ 'title' : {$regex : ?0, $options : 'i'} }")
+    List<Post> searchTitle(String text);
 
 }
