c9306991eb227cd224444b9f7057612627e20211
Pedro Bassetto <contato@pedrobassetto.com>
2020-05-12 21:19:17 -0300

    Consulta com @Query com varios criterios

--- src/main/java/com/pedrobassetto/workshopmongo/resources/PostResource.java
+++ src/main/java/com/pedrobassetto/workshopmongo/resources/PostResource.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -28,6 +29,19 @@ public class PostResource {
     public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
         text = URL.decodeParam(text);
         List<Post> obj = service.findByTitle(text);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping(value = "/fullsearch")
+    public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value = "text", defaultValue = "") String text,
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate){
+        text = URL.decodeParam(text);
+        Date dtMin = URL.convertDate(minDate, new Date(0L));
+        Date dtMax = URL.convertDate(maxDate, new Date());
+
+        List<Post> obj = service.fullSearch(text, dtMin, dtMax);
         return ResponseEntity.ok().body(obj);
     }
 }
